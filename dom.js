d20ec3242754a8975766c2d2076b9a9cec22b334
santhoshsrss <santhoshsanjaiyan699@gmail.com>
2022-12-18 18:17:28 +0530

    ddd

--- dom.js
+++ dom.js
@@ -1,2 +1,11 @@
-var a = "santhosh";
-console.log(a);
+
+function clicked(){
+var a = Math.floor(Math.random() *255)+1;
+var b = Math.floor(Math.random() *255)+1;
+var c = Math.floor(Math.random() *255)+1;
+var newColor = "rgb(" + a + "," + b + "," + c + ")";
+    console.log("submitted")
+    document.querySelector('body').style.backgroundColor = newColor
+    // document.querySelector('body').style.backgroundColor = null;
+
+}
